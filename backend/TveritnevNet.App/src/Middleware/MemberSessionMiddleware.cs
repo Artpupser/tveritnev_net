903ea9bbd45c9c72c429bffce7bd27e17de6f67b
Artpupser <artpupser@proton.me>
2026-05-21 22:08:31 +0300

    add: member session middleware, admin session middleware, abstract: user session middleware

--- backend/TveritnevNet.App/src/Middleware/MemberSessionMiddleware.cs
+++ backend/TveritnevNet.App/src/Middleware/MemberSessionMiddleware.cs
@@ -4,6 +4,6 @@ using TveritnevNet.App.Models.Database;
 
 namespace TveritnevNet.App.Middleware;
 
-public sealed class AdminSessionMiddleware(IDatabaseConnectionFactory databaseConnectionFactory) : UserSessionMiddleware(databaseConnectionFactory) {
+public sealed class MemberSessionMiddleware(IDatabaseConnectionFactory databaseConnectionFactory) : UserSessionMiddleware(databaseConnectionFactory) {
    protected override UserDatabaseRole Role => UserDatabaseRole.Admin;
 }
