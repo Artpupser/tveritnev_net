51505bf431177aea3ca369f884495fb50581807c
Neitan <kirillgovik@gmail.com>
2026-06-26 14:28:40 +0300

    add astro-compress and astro-compressor

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,24 +1,33 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
+import compress from "astro-compress";
+import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react(),
+    compress(),
+    compressor({
+      gzip: true,
+      brotli: true,
+    }),
+  ],
 
   vite: {
     plugins: [tailwindcss()],
-    
+
     server: {
       proxy: {
-        '/api': {
-          target: 'http://localhost:50501',
+        "/api": {
+          target: "http://localhost:50501",
           changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-        }
-      }
-    }
-  }
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  },
 });
